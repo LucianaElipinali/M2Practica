3ef4b1fdb897cb51199bfecb5c1ee5e3a4a2eb69
Luchi Elipinali <luelipinali@gmail.com>
2024-12-17 22:28:23 -0300

    Hola

--- front/scripts/botonLimpiar.js
+++
@@ -1,6 +0,0 @@
-const botonLimpiar = document.getElementById("botonLimpiar");
-
-botonLimpiar.addEventListener("click", () =>{
-    const movieForm = document.getElementById("movieForm");
-    movieForm.reset();
-})
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,12 +1,1 @@
-const renderCards = require("./renderCards");
-const axios = require("axios");
-
-const renderMovies = async () => {
-    try {
-        const response = await axios.get("http://localhost:3000/movies");
-        renderCards(response.data);
-    } catch (error) {
-        throw new Error("Error al obtener las películas", error.message)
-    }
-};
-renderMovies();
+console.log(tempData);
