5d32040f326410a5989f25e984f79a8b5798e81d
Luchi Elipinali <luelipinali@gmail.com>
2024-12-21 11:55:28 -0300

    1era parte de js y css listos

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,58 @@
 console.log(tempData);
+
+const moviesContainer = document.getElementById("movies-container");
+
+tempData.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    const movieTitle = document.createElement("h3");
+    movieTitle.textContent = movie.title;
+    const movieImage = document.createElement("img");
+    movieImage.src = movie.poster;
+    movieImage.alt = movieTitle;
+
+    movieCard.appendChild(movieTitle);
+    movieCard.appendChild(movieImage);
+
+    moviesContainer.appendChild(movieCard);
+
+    movieCard.addEventListener("click", () => {
+        movieCard.classList.toggle("selected");
+    
+        if (movieCard.classList.contains("selected")) {
+            const movieDetails = document.createElement("span");
+            movieDetails.classList.add("movie-details");
+    
+            const movieYear = document.createElement("p");
+            movieYear.textContent = `Year: ${movie.year}`;
+    
+            const movieDirector = document.createElement("p");
+            movieDirector.textContent = `Director: ${movie.director}`;
+    
+            const movieDuration = document.createElement("p");
+            movieDuration.textContent = `Duration: ${movie.duration}`;
+    
+            const movieGenre = document.createElement("p");
+            movieGenre.textContent = `Genre: ${movie.genre.join(", ")}`;
+    
+            const movieRate = document.createElement("p");
+            movieRate.textContent = `Rate: ${movie.rate}`;
+    
+            movieDetails.appendChild(movieYear);
+            movieDetails.appendChild(movieDirector);
+            movieDetails.appendChild(movieDuration);
+            movieDetails.appendChild(movieGenre);
+            movieDetails.appendChild(movieRate);
+    
+            movieCard.appendChild(movieDetails);
+        }
+    
+        if (!movieCard.classList.contains("selected")) {
+            const movieDetails = movieCard.querySelector(".movie-details");
+            if (movieDetails) {
+                movieDetails.remove();
+            }
+        }
+    });
+    
+});
